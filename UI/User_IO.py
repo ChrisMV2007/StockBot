a19a4104ac301d9e99f4e8f40b739f00699fc069
Christopher Vu <107224696+ChrisMV2007@users.noreply.github.com>
2022-09-08 08:06:26 -0700

    Major user IO update

--- UI/User_IO.py
+++ UI/User_IO.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import Backend.Indicators as Indicators
+import Graph as graph
 import csv
 
 
@@ -23,25 +25,30 @@ def inp(ques, ans=None, int_only=False, yn=False, rep_msg=None, rep=False, no_an
 
 def new_user(df, path):
     user = input('What would you like your username to be? ')
+
     if user in list(df['User']):
         print('This username is taken!')
         new_user(df, path)
+
     else:
-        row = [f'{user}', f'info {user}']
+        row = df.loc[df['User'] == 'Default']
+        row['User'] = row['User'].replace(['Default'], f'{user}')
         newuser = pd.DataFrame(row)
-        users = pd.concat([df, newuser], ignore_index=True)
+        users = pd.concat([df, newuser.iloc[0]], ignore_index=True)
         with open(path, 'a') as users:
             writer = csv.writer(users)
-            writer.writerow(row)
+            writer.writerow(newuser.iloc[0])
         data = pd.read_csv('UsersandSettings.csv', encoding="windows_1258")
         return data.loc[data['User'] == user]
 
 
 def login(df, path):
     user = input('What is your username? ')
+
     if user not in list(df['User']):
         print('This user does not exist!')
         login(df, path)
+
     else:
         data = pd.read_csv('UsersandSettings.csv', encoding="windows_1258")
         return data.loc[data['User'] == user]
@@ -49,7 +56,6 @@ def login(df, path):
 
 def login_signup():
     users = pd.read_csv('UsersandSettings.csv', encoding="windows_1258")
-
     los = inp('Would you like to log in or sign up (log in, sign up)? ', ['log in', 'sign up'],
               rep_msg='Please enter either "log in" or "sign up"')
 
@@ -58,9 +64,41 @@ def login_signup():
     if los == 'log in':
         userinfo = login(users, 'UsersandSettings.csv')
 
-def autograph(hist, userinfo):
+    return userinfo
 
 
+def try_replace(x):
+    try:
+        return int(x)
+    except:
+        return x
+
+
+def auto_graph(hist, stockname, userinfo):
+    inames = [ind for ind in ['SMA', 'EMA', 'Stochastic RSI', 'RSI'] if ind in userinfo['Def_Indicators']]
+
+    if 'EMA' in inames:
+        inames.remove('EMA')
+        inames.append('EMA')
+    if 'SMA' in inames:
+        inames.remove('SMA')
+        inames.append('SMA')
+
+    indicators = []
+    colors = []
+    for i in inames:
+        idict = {'SMA': Indicators.sma, 'EMA': Indicators.ema, 'RSI': Indicators.rsi,
+                 'Stochastic RSI': Indicators.stochastic_rsi}
+
+        isettings = list(map(try_replace(try_replace, userinfo[f'Def_{i}_Set'].split(','))))
+        indicators.append(idict[i](var_iter=[hist] + [isettings]))
+
+        col = userinfo[f'Def_{i}_Col']
+        colors.append(col if ',' not in col else col.split(','))
+
+    graph.graph(stock=stockname, hist=hist, type=userinfo['Def_Gtype'],
+                dark_mode=True if userinfo['DarkMode'] == 'Y' else False, indicators=indicators, inames=inames,
+                ema_sma_w_hist=True if userinfo['MAwHist'] == 'Y' else False, icolors=colors)
 
 
 if __name__ == '__main__':
