76d12d0f6b3a431ff1f60e18bccdda164b1547d2
Christopher Vu <107224696+ChrisMV2007@users.noreply.github.com>
2022-10-09 09:21:36 -0700

    Add files via upload

---
+++ UI/main_func.py
@@ -0,0 +1,16 @@
+import User_IO as UI
+
+
+def main():
+    while True:
+        UI.login_cycle()
+        cont = UI.inp('>>> Would you like to log in or exit the program ("log in", "exit")? ', ans=['log in', 'exit'],
+                      rep_msg='Please input "log in" or "exit".')
+        if cont == 'log in':
+            UI.login_cycle()
+        if cont == 'exit':
+            return
+
+
+if __name__ == '__main__':
+    main()
