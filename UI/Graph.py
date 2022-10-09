26bc9e619c1d53e93097731f325d1f3822cd8870
Christopher Vu <107224696+ChrisMV2007@users.noreply.github.com>
2022-10-08 17:03:21 -0700

    Update Graph.py

--- UI/Graph.py
+++ UI/Graph.py
@@ -17,55 +17,96 @@ class invalid_type(Exception):
 # IMPORTANT NOTE: IF EMA_SMA_W_HIST IS SET TO TRUE, SMA AND EMA MUST BE INPUT AFTER RSI AND STOCHASTIC RSI IN THE
 # INDICATORS LIST
 def graph(stock, hist, type, dark_mode=False, indicators=[], inames=[], ema_sma_w_hist=False, icolors=[]):
-    if indicators=='NA_':
-        indicators=[]
-    
+    if indicators == 'NA_':
+        indicators = []
+
+    ema_sma_w_hist = True if ema_sma_w_hist == 'yes' else False
+
     if type not in ['line', 'candles']:
         raise invalid_type(type, 'display stock history', ['line', 'candles'])
     if dark_mode:
         plt.style.use('dark_background')
 
     fig, ax = plt.subplots(
-        nrows=len([x for x in inames if x not in ['EMA', 'SMA']]) + 1 if ema_sma_w_hist else len(indicators) + 1,
+        nrows=len([x for x in inames if x not in ['ema', 'sma']]) + 1 if ema_sma_w_hist else len(indicators) + 1,
         ncols=1)
 
-    ax[0].set_xlabel('Date')
-    ax[0].set_ylabel('Price')
-    ax[0].set_title(f'{stock} {type[:1].capitalize() + type[1:]} Graph')
+    axes = False
+    if len(fig.get_axes()) > 1:
+        axes = True
 
-    hist['Date'] = pd.to_datetime(hist['Date'])
-    date_format = mpdates.DateFormatter('%Y-%m-%d')
-    ax[0].xaxis.set_major_formatter(date_format)
+    if axes:
+        ax[0].set_xlabel('Date')
+        ax[0].set_ylabel('Price')
+        ax[0].set_title(f'{stock} {type[:1].capitalize() + type[1:]} Graph')
+        hist['Date'] = pd.to_datetime(hist['Date'])
+        date_format = mpdates.DateFormatter('%Y-%m-%d')
+        ax[0].xaxis.set_major_formatter(date_format)
 
-    if type == 'line':
-        ax[0].fill_between(hist['Date'], hist['Close'], where=hist['Close'] > 0, alpha=0.25, color='b',
-                           interpolate=True)
-        ax[0].plot(hist['Date'], hist['Close'])
+        if type == 'line':
+            ax[0].fill_between(hist['Date'], hist['Close'], where=hist['Close'] > 0, alpha=0.25, color='b',
+                               interpolate=True)
+            ax[0].plot(hist['Date'], hist['Close'])
 
-    if type == 'candles':
-        copy = hist.copy()
-        copy['Date'] = copy['Date'].map(mpdates.date2num)
-        candlestick_ohlc(ax[0], copy.values, width=0.6, colorup='green', colordown='red', alpha=0.8)
+        if type == 'candles':
+            copy = hist.copy()
+            copy['Date'] = copy['Date'].map(mpdates.date2num)
+            candlestick_ohlc(ax[0], copy.values, width=0.6, colorup='green', colordown='red', alpha=0.8)
 
-    for index, indicator in enumerate(indicators):
-        index, color = index, icolors[index]
-        label = inames[index]
-        if label == 'Stochastic RSI':
-            ax[index + 1].plot(hist['Date'][len(hist) - len(indicator[0]):], indicator[0], color=color[0],
-                               label=label)
-            ax[index + 1].plot(hist['Date'][len(hist) - len(indicator[1]):], indicator[1], color=color[1],
-                               label=label)
-            ax[index + 1].fill_between(hist['Date'], -20, 20, color=color[0], alpha=0.25)
-            ax[index + 1].fill_between(hist['Date'], 80, 120, color=color[1], alpha=0.25)
-        if inames[index] in ['EMA', 'SMA'] and ema_sma_w_hist:
-            index = -1
-        else:
-            ax[index + 1].set_ylabel(inames[index])
-        if label == 'RSI':
-            ax[index + 1].fill_between(hist['Date'], 10, 30, color=color, alpha=0.25)
-            ax[index + 1].fill_between(hist['Date'], 70, 90, color=color, alpha=0.25)
-        if label != 'Stochastic RSI':
-            ax[index + 1].plot(hist['Date'][len(hist) - len(indicator):], indicator, color=color, label=label)
+        for index, indicator in enumerate(indicators):
+            index, color = index, icolors[index]
+            label = inames[index]
+            if label == 'Stochastic RSI':
+                ax[index + 1].plot(hist['Date'][len(hist) - len(indicator[0]):], indicator[0], color=color[0],
+                                   label=label)
+                ax[index + 1].plot(hist['Date'][len(hist) - len(indicator[1]):], indicator[1], color=color[1],
+                                   label=label)
+                ax[index + 1].fill_between(hist['Date'], -20, 20, color=color[0], alpha=0.25)
+                ax[index + 1].fill_between(hist['Date'], 80, 120, color=color[1], alpha=0.25)
+            if inames[index] in ['EMA', 'SMA'] and ema_sma_w_hist:
+                index = -1
+            else:
+                ax[index + 1].set_ylabel(inames[index])
+            if label == 'RSI':
+                ax[index + 1].fill_between(hist['Date'], 10, 30, color=color, alpha=0.25)
+                ax[index + 1].fill_between(hist['Date'], 70, 90, color=color, alpha=0.25)
+            if label != 'Stochastic RSI':
+                ax[index + 1].plot(hist['Date'][len(hist) - len(indicator):], indicator, color=color, label=label)
+
+    else:
+        hist['Date'] = pd.to_datetime(hist['Date'])
+        date_format = mpdates.DateFormatter('%Y-%m-%d')
+        ax[0].xaxis.set_major_formatter(date_format)
+
+        if type == 'line':
+            ax[0].fill_between(hist['Date'], hist['Close'], where=hist['Close'] > 0, alpha=0.25, color='b',
+                               interpolate=True)
+            ax[0].plot(hist['Date'], hist['Close'])
+
+        if type == 'candles':
+            copy = hist.copy()
+            copy['Date'] = copy['Date'].map(mpdates.date2num)
+            candlestick_ohlc(ax[0], copy.values, width=0.6, colorup='green', colordown='red', alpha=0.8)
+
+        for index, indicator in enumerate(indicators):
+            index, color = index, icolors[index]
+            label = inames[index]
+            if label == 'Stochastic RSI':
+                ax[index + 1].plot(hist['Date'][len(hist) - len(indicator[0]):], indicator[0], color=color[0],
+                                   label=label)
+                ax[index + 1].plot(hist['Date'][len(hist) - len(indicator[1]):], indicator[1], color=color[1],
+                                   label=label)
+                ax[index + 1].fill_between(hist['Date'], -20, 20, color=color[0], alpha=0.25)
+                ax[index + 1].fill_between(hist['Date'], 80, 120, color=color[1], alpha=0.25)
+            if inames[index] in ['EMA', 'SMA'] and ema_sma_w_hist:
+                index = -1
+            else:
+                ax[index + 1].set_ylabel(inames[index])
+            if label == 'RSI':
+                ax[index + 1].fill_between(hist['Date'], 10, 30, color=color, alpha=0.25)
+                ax[index + 1].fill_between(hist['Date'], 70, 90, color=color, alpha=0.25)
+            if label != 'Stochastic RSI':
+                ax[index + 1].plot(hist['Date'][len(hist) - len(indicator):], indicator, color=color, label=label)
 
     fig.autofmt_xdate()
     fig.tight_layout()
