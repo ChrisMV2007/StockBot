4ecf3518a20bb507f26eaef54253033bc4f439e0
Christopher Vu <107224696+ChrisMV2007@users.noreply.github.com>
2022-10-24 07:48:37 -0700

    Update IndicatorAnalysis.py

--- UI/Backend/IndicatorAnalysis.py
+++ UI/Backend/IndicatorAnalysis.py
@@ -1,14 +1,37 @@
 def rsi_anal(rsi, bound):
-    pass
+    if bound[0] == '>':
+        return rsi[rsi.size - 1] > bound[1:]
+    if bound[0] == '<':
+        return rsi[rsi.size - 1] < bound[1:]
 
 
-def stochastic_rsi_anal(stochastic_rsi, bound):
-    pass
+def stochastic_rsi_anal(stochastic_rsi, bound, KoD):  # K comes first
+    if KoD.len == 2:
+        return True if eval(
+            f'{stochastic_rsi[0][stochastic_rsi[0].size - 1]} {bound[0][0]} {bound[0][1:]}') and eval(
+            f'{stochastic_rsi[1][stochastic_rsi[1].size - 1]} {bound[1][0]} {bound[1][1:]}') else False
+    if KoD.len == 1:
+        if KoD == 'K':
+            if bound[0][0] == '>':
+                return stochastic_rsi[0][stochastic_rsi[0].size - 1] > bound[0][1:]
+            if bound[0][0] == '<':
+                return stochastic_rsi[0][stochastic_rsi[0].size - 1] < bound[0][1:]
+        if KoD == 'D':
+            if bound[1][0] == '>':
+                return stochastic_rsi[1][stochastic_rsi[1].size - 1] > bound[0][1:]
+            if bound[1][0] == '<':
+                return stochastic_rsi[1][stochastic_rsi[1].size - 1] < bound[0][1:]
 
 
 def ema_anal(ema, bound):
-    pass
+    if bound[0] == '>':
+        return ema[ema.size - 1] > bound[1:]
+    if bound[0] == '<':
+        return ema[ema.size - 1] < bound[1:]
 
 
 def sma_anal(sma, bound):
-    pass
+    if bound[0] == '>':
+        return sma[sma.size - 1] > bound[1:]
+    if bound[0] == '<':
+        return sma[sma.size - 1] < bound[1:]
