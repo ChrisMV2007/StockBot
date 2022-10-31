a4284b0dfacd29c2bc384babe3ebef40b0d35833
Christopher Vu <107224696+ChrisMV2007@users.noreply.github.com>
2022-10-30 17:08:09 -0700

    Update IndicatorAnalysis.py

--- UI/Backend/IndicatorAnalysis.py
+++ UI/Backend/IndicatorAnalysis.py
@@ -1,8 +1,8 @@
 def rsi_anal(rsi, bound):
     if bound[0] == '>':
-        return rsi[rsi.size - 1] > bound[1:]
+        return rsi[rsi.size - 1] > float(bound[1:])
     if bound[0] == '<':
-        return rsi[rsi.size - 1] < bound[1:]
+        return rsi[rsi.size - 1] < float(bound[1:])
 
 
 def stochastic_rsi_anal(stochastic_rsi, bound, KoD):  # K comes first
@@ -13,18 +13,18 @@ def stochastic_rsi_anal(stochastic_rsi, bound, KoD):  # K comes first
     if KoD.len == 1:
         if KoD == 'k':
             if bound[0][0] == '>':
-                return stochastic_rsi[0][stochastic_rsi[0].size - 1] > bound[0][1:]
+                return stochastic_rsi[0][stochastic_rsi[0].size - 1] > float(bound[0][1:])
             if bound[0][0] == '<':
-                return stochastic_rsi[0][stochastic_rsi[0].size - 1] < bound[0][1:]
+                return stochastic_rsi[0][stochastic_rsi[0].size - 1] < float(bound[0][1:])
         if KoD == 'd':
             if bound[1][0] == '>':
-                return stochastic_rsi[1][stochastic_rsi[1].size - 1] > bound[0][1:]
+                return stochastic_rsi[1][stochastic_rsi[1].size - 1] > float(bound[0][1:])
             if bound[1][0] == '<':
-                return stochastic_rsi[1][stochastic_rsi[1].size - 1] < bound[0][1:]
+                return stochastic_rsi[1][stochastic_rsi[1].size - 1] < float(bound[0][1:])
 
 
 def ma_anal(ma, bound, price):  # input price as a % above or below
     if bound[0] == '>':
-        return (ma[ma.size - 1] - price) / price > bound[1:] / 100
+        return (ma[ma.size - 1] - price) / price > float(bound[1:]) / 100
     if bound[0] == '<':
-        return (ma[ma.size - 1] - price) / price < bound[1:] / 100
+        return (ma[ma.size - 1] - price) / price < float(bound[1:]) / 100
